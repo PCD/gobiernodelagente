ffa54cfddfde0a7e5530f13b9816490cea9bf902
Cristian Cienfuegos <cristian100@gmail.com>
2013-09-15 00:04:57 -0600

    fixed tabs

--- themes/prensa/js/prensa.js
+++ themes/prensa/js/prensa.js
@@ -2,7 +2,22 @@
   Drupal.behaviors.prensa = {
     attach: function (context, settings) {
       $('document', context).ready(function(){
-        //$('#recent-articles-tabs', context).tabs();
+        $('#recent-articles-tabs ul li a', context).click(function(event){
+          if ( $(this).parent().hasClass('active') ) {
+            // Ignore
+          } else {
+            // Switch Selected Tab
+            current = $('#recent-articles-tabs ul li.active a').attr('href');
+            $('#recent-articles-tabs ul li.active').removeClass('active');
+            $(this).parent().addClass('active');
+            
+            // Switch Active Content
+            next = $(this).attr('href');
+            $(next).addClass('active');
+            $(current).removeClass('active');
+          }
+          event.preventDefault();
+        });
       });
       $('.group-image-source span.file a').attr('target', '_blank');
     }
